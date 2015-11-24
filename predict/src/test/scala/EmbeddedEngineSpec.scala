baf7361b0e4475a17bd750eed4fbf4655832c1b2
Stefan Krawczyk <stefan@idibon.com>
2015-11-23 19:54:08 -0800

    Adding intellij to gradle
    Adding idea plugin to gradle. Also updateing .gitignore.
    Apparently I also updated the gradlewrapper so Im not sure
    what that is about but it seems sensible. Also trying out a
    pre-commit hook.

--- predict/src/test/scala/EmbeddedEngineSpec.scala
+++ predict/src/test/scala/EmbeddedEngineSpec.scala
@@ -7,7 +7,7 @@ class EmbeddedEngineSpec extends FunSpec with Matchers {
   describe("EmbeddedEngine") {
 
     it("should call the constructor correctly") {
-      val engine = new EmbeddedEngine
+      val engine = new EmbeddedEngine()
       engine shouldBe an[Engine]
     }
 
