05ba0c6839e8b4d9fe73b6696e6c15f5b1fb1fbe
Gary King <gary@idibon.com>
2015-12-29 16:55:48 -0800

    tokenizer: port TokenTransformer to use ICUTokenizer
    delete the throw-away tokenizer and modify the TokenTransformer
    class to build off of ICUTokenizer. since this makes the mis-
    leading filename completely wrong, now, rename the file to
    reflect the exported class
    
    TBD:
     * bypass language detection if the language is known (maybe
       by adding a variadic parameter to apply(), so that it is
       treated as optional)
     * handle HTML

--- predict/src/main/scala/com/idibon/ml/feature/tokenizer/TokenTransformer.scala
+++ predict/src/main/scala/com/idibon/ml/feature/tokenizer/TokenTransformer.scala
@@ -5,17 +5,8 @@ import com.idibon.ml.feature.{Feature,FeatureTransformer}
 
 package com.idibon.ml.feature.tokenizer {
 
-  /** Internal implementation of the tokenization transformation */
-  private[tokenizer] class Tokenizer {
-
-    /** Partitions an input content string into the tokens it contains */
-    def tokenize(content: StringLike[_]): Seq[Token] = {
-      content.split(' ').map(t => new Token(t, Tag.Word, 0, 0))
-    }
-  }
-
   /** Tokenization FeatureTransformer */
-  class TokenTransformer extends Tokenizer with FeatureTransformer {
+  class TokenTransformer extends FeatureTransformer {
 
     /** Tokenizes an array of strings stored in the "content" key of the map
       *
@@ -30,7 +21,7 @@ package com.idibon.ml.feature.tokenizer {
       // use foldLeft rather than reduce to handle the empty-list case
       contents.foldLeft(List[Token]())((tokens, content) => {
         // append the tokens in content to the accumulated list of tokens
-        tokens ++ tokenize(content.get)
+        tokens ++ ICUTokenizer.tokenize(content.get)
       })
     }
   }
