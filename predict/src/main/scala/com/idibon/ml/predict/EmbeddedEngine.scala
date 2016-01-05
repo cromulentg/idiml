e937e762de4d637ef8ad6d9d28dc4338f00ec666
Stefan Krawczyk <stefan@idibon.com>
2016-01-05 11:20:04 -0800

    Predict example now uses feature pipeline builder
    This updates the predict example to use the feature pipeline
    builder to create a feature vector.

--- predict/src/main/scala/com/idibon/ml/predict/EmbeddedEngine.scala
+++ predict/src/main/scala/com/idibon/ml/predict/EmbeddedEngine.scala
@@ -2,13 +2,15 @@ package com.idibon.ml.predict
 
 import com.idibon.ml.alloy.IntentAlloy
 import com.idibon.ml.feature.bagofwords.BagOfWordsTransformer
+import com.idibon.ml.feature.indexer.IndexTransformer
 import com.idibon.ml.feature.tokenizer.TokenTransformer
-import com.idibon.ml.feature.{FeaturePipeline, StringFeature}
+import com.idibon.ml.feature.{FeatureTransformer, FeaturePipelineBuilder, FeaturePipeline, StringFeature}
 import com.idibon.ml.predict.ml.IdibonLogisticRegressionModel
 import org.apache.spark.ml.classification.IdibonSparkLogisticRegressionModelWrapper
 import org.apache.spark.mllib.feature
 import org.json4s._
-import org.json4s.native.JsonMethods._
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods.{compact, parse, render}
 
 
 /**
@@ -22,52 +24,38 @@ class EmbeddedEngine extends Engine {
     */
   def start() = {
     println("Called from Scala")
-
+    // create pipeline manually
+    val pipeline: FeaturePipeline = (FeaturePipelineBuilder.named("StefansPipeline")
+      += (FeaturePipelineBuilder.entry("convertToIndex", new IndexTransformer, "bagOfWords"))
+      += (FeaturePipelineBuilder.entry("bagOfWords", new BagOfWordsTransformer, "convertToTokens"))
+      += (FeaturePipelineBuilder.entry("convertToTokens", new TokenTransformer, "contentExtractor"))
+      += (FeaturePipelineBuilder.entry("contentExtractor", new DocumentExtractor, "$document"))
+      := ("convertToIndex"))
     // get some data.
     val text: String = "Everybody loves replacing hadoop with spark because it's much faster. a b d"
-    val rawFeature = new StringFeature(text)
-    // do some featurization
-    val tokenizer = new TokenTransformer()
-    val tokenized = tokenizer.apply(Seq[StringFeature](rawFeature))
-    println(tokenized)
-    val bOWer = new BagOfWordsTransformer()
-    val bagOfWords = bOWer.apply(tokenized)
-    println(bagOfWords)
-    // convert to indexes
-    val hashingTF = new feature.HashingTF()
-    val hashedIndexes = hashingTF.transform(bagOfWords)
-    println(hashedIndexes)
+    val doc: JObject = ( "content" -> text )
+    // create feature vector
+    val featureVector = pipeline(doc).head
+    println(featureVector)
 
     // load some stock model
-    val weights = hashedIndexes  // Just using these as a placeholder.
+    val weights = featureVector  // Just using these as a placeholder.
     val intercept = 0.0
     val sparkLRModel = new IdibonSparkLogisticRegressionModelWrapper("myModel", weights, intercept)
     val idibonModel = new IdibonLogisticRegressionModel()
     idibonModel.lrm = sparkLRModel
 
-    println(sparkLRModel.predictProbability(hashedIndexes))
-    println(idibonModel.predict(hashedIndexes, false, 0.0).toString)
+    println(sparkLRModel.predictProbability(featureVector))
+    println(idibonModel.predict(featureVector, false, 0.0).toString)
   }
 
-  /**
-    * Next iteration of the above. WIP. Need Gary's branch...
-    * @return
-    */
-  def realisticExample() = {
-    val json = parse(
-      """{
-"transforms":[
-  {"name":"A","class":"com.idibon.ml.feature.tokenizer.TokenTransformer"},
-  {"name":"B","class":"com.idibon.ml.feature.bagofwords.BagOfWordsTransformer"},
-  {"name":"C","class":"com.idibon.ml.feature.indexer.IndexTransformer"}],
-"pipeline":[
-  {"name":"A","inputs":["$document"]},
-  {"name":"B","inputs":["A"]},
-  {"name":"C","inputs":["B"]},
-  {"name":"$output","inputs":["C"]}]}""").asInstanceOf[JObject]
-    val pipeline = new FeaturePipeline()
-    val reader = new IntentAlloy() //TODO create pipeline via "builder"
-    pipeline.load(reader.reader(), Some(json.asInstanceOf[JObject]))
-  }
+}
 
+/**
+  * Remove this class after Michelle's commit.
+  */
+class DocumentExtractor extends FeatureTransformer {
+  def apply(document: JObject): Seq[StringFeature] = {
+    List(StringFeature((document \ "content").asInstanceOf[JString].s))
+  }
 }
