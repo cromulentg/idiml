b613230283722702166cb774d7b5e4a1c0543a51
Michelle Casbon <michelle@idibon.com>
2016-01-06 13:50:44 -0800

    ENG-2908: Handle dynamic lable names
    Remove debug code
    Use Codec.String.read/write for loading/saving the pipeline
    Handle positive and negative annotations

--- predict/src/main/scala/com/idibon/ml/feature/DocumentExtractor.scala
+++ predict/src/main/scala/com/idibon/ml/feature/DocumentExtractor.scala
@@ -2,9 +2,7 @@ package com.idibon.ml.feature
 
 import org.json4s._
 
-/**
-  * Created by michelle on 12/28/15.
-  */
+/** Extracts document content from a JObject and turns it into a list of StringFeatures */
 class DocumentExtractor extends FeatureTransformer {
   def apply(document: JObject): Seq[StringFeature] = {
     List(StringFeature((document \ "content").asInstanceOf[JString].s))
