8913f52f9fac9bd040815abda2ecf4cb4790c648
Michelle Casbon <michelle@idibon.com>
2016-01-06 18:33:48 -0800

    ENG-2908: Separate RDD generation into its own module
    Fix a few build dependencies
    Repair an indexer spec & add a new one
    Instantiate a SparkContext and generate RDDs of LabeledPoints for training

--- predict/src/test/scala/com/idibon/ml/feature/indexer/IndexerSpec.scala
+++ predict/src/test/scala/com/idibon/ml/feature/indexer/IndexerSpec.scala
@@ -54,17 +54,27 @@ class IndexerSpec extends FunSpec with Matchers with BeforeAndAfter {
         new Token("ideas", Tag.Word, 0, 0), new Token("sleep", Tag.Word, 0, 0),
         new Token("sleep", Tag.Word, 0, 0), new Token("furiously", Tag.Word, 0, 0),
         new Token("green", Tag.Word, 0, 0))
-      val result1 = transform.apply(fiveTokens)
+      transform.apply(fiveTokens)
 
       // Save the results
       val intentAlloy = new IntentAlloy()
       transform.save(intentAlloy.writer)
 
       // Load the results
-      val transform2 = (new IndexTransformLoader).load(intentAlloy.reader, null)
-      val result2 = transform2.apply(fiveTokens)
+      val transform2 = new IndexTransformer()
+      transform2.load(intentAlloy.reader, null)
 
-      result1 shouldBe result2
+      transform.getFeatureIndex shouldBe transform2.getFeatureIndex
+    }
+
+    it("should give the same result after applying twice") {
+      val fiveTokens = Seq[Feature[Token]](
+        new Token("colorless", Tag.Word, 0, 1), new Token("green", Tag.Word, 1, 1),
+        new Token("ideas", Tag.Word, 0, 1), new Token("sleep", Tag.Word, 1, 1),
+        new Token("furiously", Tag.Word, 1, 1))
+      val expected = Vectors.sparse(5, Seq((0, 1.0), (1, 1.0), (2, 1.0), (3, 1.0), (4, 1.0)))
+      transform.apply(fiveTokens) shouldBe expected
+      transform.apply(fiveTokens) shouldBe expected
     }
   }
 }
