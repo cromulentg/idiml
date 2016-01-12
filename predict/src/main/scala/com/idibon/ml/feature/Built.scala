ea499dce6e6b3dc65dad69f9691fb3b777fe6779
Gary King <gary@idibon.com>
2016-01-11 18:55:53 -0800

    idiml-features: add new interface for feature saving
    using paired class parameters for builders works well in theory; however,
    if you try to define a generic feature transformer, like the IndexTransformer,
    this leads to awful type signatures like:
    
      def apply(features: Seq[Feature[_, _, _]])
    
    or worse, for the tokenizer:
    
      def apply(features: Seq[Feature[String, _, _]])
    
    where the first _ refers to the underlying raw type of the feature, the
    second _ is the recursive type, and the third _ is the Builder type (or any
    other order of those 3 parameters)
    
    rather than allowing these types to get out of hand, move the conjoined
    parameters for building into a separate trait, so that transformers can
    just refer to "Feature[x]" unless the transform actually needs to save
    the feature

---
+++ predict/src/main/scala/com/idibon/ml/feature/Built.scala
@@ -0,0 +1,26 @@
+package com.idibon.ml.feature
+
+import java.io.{DataInputStream, DataOutputStream}
+
+/** Mixin trait for features that support saving and re-loading
+  *
+  * Implementations are paired to Builder classes that read data from an
+  * InputStream and instantiate the object, so that objects may be
+  * immutable.
+  */
+trait Built[T <: Built[T, Builder[T]], +U <: Builder[T]] {
+
+  /** Stores the data to an output stream so it may be reloaded later.
+    *
+    * @param output - Output stream to write data
+    */
+  def save(output: DataOutputStream)
+}
+
+trait Builder[T] {
+  /** Instantiates and loads an object from an input stream
+    *
+    * @param input - Data stream where object was previously saved
+    */
+  def build(input: DataInputStream): T
+}
