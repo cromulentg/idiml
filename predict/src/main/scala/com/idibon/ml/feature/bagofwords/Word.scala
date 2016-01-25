ff19d3c0a422a4448ae37f482e9f19cd5ade70ef
Gary King <gary@idibon.com>
2016-01-25 09:42:21 -0800

    bagofwords: make BagOfWords more useful
    make a bunch of changes to BagOfWords to redefine its general
    behavior so that it is a useful pre-processor for N-Grams and skip-
    grams (or a useful feature for use in models directly)
    
     * remove counting, and instead return all tokens in-order. the counts
       generally aren't wanted for models, since it increases dimensionality
       (happy, 2) is a different feature for the index than (happy, 1), so
       we would need special handling to undo this transform. counts also
       make it impossible to use BagOfWords as a preprocessor for n-grams,
       since the ordering is lost. if we want a uniquifier / counter
       transform, we should implement a generic one that works for all
       input feature types
    
     * add configuration arguments for performing both token filtering (e.g.,
       discarding whitespace) and case-folding (currently only "None" is
       defined, but we can define other transforms in the future)
    
     * add a LanguageCode argument to transform, so that case-folding rules,
       if used, can be language-sensitive.

--- predict/src/main/scala/com/idibon/ml/feature/bagofwords/Word.scala
+++ predict/src/main/scala/com/idibon/ml/feature/bagofwords/Word.scala
@@ -3,10 +3,10 @@ package com.idibon.ml.feature.bagofwords
 import com.idibon.ml.alloy.Codec
 import com.idibon.ml.feature._
 
-/** This class represents a word and it's occurrence count.
+/** This class represents a word
   * @author Stefan Krawczyk <stefan@idibon.com>
   */
-case class Word(word: String, count: Int) extends Feature[Word]
+case class Word(word: String) extends Feature[Word]
     with Buildable[Word, WordBuilder] {
 
   def get = this
@@ -14,7 +14,6 @@ case class Word(word: String, count: Int) extends Feature[Word]
   /** Stores the feature to an output stream so it may be reloaded later. */
   override def save(output: FeatureOutputStream): Unit = {
     Codec.String.write(output, word)
-    Codec.VLuint.write(output, count)
   }
 }
 
@@ -22,7 +21,6 @@ class WordBuilder extends Builder[Word] {
   /** Reloads a previously saved feature */
   override def build(input: FeatureInputStream): Word = {
     val word = Codec.String.read(input)
-    val count = Codec.VLuint.read(input)
-    new Word(word, count)
+    new Word(word)
   }
 }
