1fbf985b070c2f297232d64ee3b8bc803860ed11
hmost1 <haleymost1@gmail.com>
2016-01-28 22:10:48 -0800

    Address PR comments
    Refactor code to be more readable, as well as
    update tests and and more inline comments.
    
    For efficiency, convert input to an IndexedSequence
    before any random access indexing.

--- predict/src/test/scala/com/idibon/ml/feature/skipgram/SkipgramTransformerSpec.scala
+++ predict/src/test/scala/com/idibon/ml/feature/skipgram/SkipgramTransformerSpec.scala
@@ -2,64 +2,69 @@ package com.idibon.ml.feature.skipgram
 
 import com.idibon.ml.feature.{StringFeature, ProductFeature}
 import org.scalatest.{Matchers, FunSpec}
-import com.idibon.ml.feature.tokenizer.{Tag,Token}
+import com.idibon.ml.feature.tokenizer.{Token}
 
 
-
-/**
-  * Created by haley on 1/26/16.
-  */
 class SkipgramTransformerSpec extends FunSpec with Matchers{
+  val transform = new SkipgramTransformer(2, 3)
+  val transform2 = new SkipgramTransformer(3,0)
+  val transform3 = new SkipgramTransformer(2,3)
+  val transform4 = new SkipgramTransformer(0,3)
+  val transform5 = new SkipgramTransformer(2,7)
+
+  val tokens = Vector(StringFeature("Insurgents"), StringFeature("killed"),
+    StringFeature("in"), StringFeature("ongoing"),StringFeature("fighting"))
+
+  describe("getSkipPermutations"){
+    it("there should be none if n is 0") {
+      transform2.getSkipPermutations() shouldBe empty
+    }
+
+    it("should should generate the correct permutations") {
+      transform3.getSkipPermutations() shouldBe List(
+        List(0, 0), List(0, 1), List(1, 0),
+        List(0, 2), List(2, 0), List(1, 1),
+        List(1, 2), List(2, 1), List(2, 2)
+      )
+    }
+  }
 
   describe("apply") {
-    //TODO: going to need to try some other indices
-    val transform = new SkipgramTransformer(2, 2)
-    val transform2 = new SkipgramTransformer(3,0)
-    val transform3 = new SkipgramTransformer(2,3)
-    val transform4 = new SkipgramTransformer(0,2)
-
-    val short_tokens = Vector(Token("paper", Tag.Word, 0, 1), Token("mache", Tag.Word, 2, 5))
-    val long_tokens = Vector(StringFeature("Twas"), StringFeature("brillig"),
-      StringFeature(","), StringFeature("and"),StringFeature("the"),
-      StringFeature("slithy"), StringFeature("toves"))
 
     it("should work on an empty sequence") {
-      //TODO: should also return empty if k or n are longer than the sequence length
       transform(Seq[Token]()) shouldBe empty
     }
 
     it("should return the empty set when n=0"){
-      transform2(short_tokens) shouldBe empty
+      transform2(tokens) shouldBe empty
     }
 
     it("should return the empty set when min(n,k) is larger than the input"){
-      transform3(short_tokens) shouldBe empty
+      transform5(tokens) shouldBe empty
     }
 
-
     it("should act like ngrams when k=0"){
-      transform4(short_tokens) shouldBe List(
-        ProductFeature(Seq(short_tokens(0),(short_tokens(1)))))
+      //tri-grams
+      transform4(tokens) shouldBe List(
+        ProductFeature(List(tokens(0), tokens(1), tokens(2))),
+        ProductFeature(List(tokens(1), tokens(2), tokens(3))),
+        ProductFeature(List(tokens(2), tokens(3), tokens(4)))
+      )
     }
 
     it("should generate a correct sequence of skipgrams") {
-
-      transform(long_tokens) shouldBe List(
-        ProductFeature(List(long_tokens(0), long_tokens(1))),
-        ProductFeature(List(long_tokens(0), long_tokens(2))),
-        ProductFeature(List(long_tokens(0), long_tokens(3))),
-        ProductFeature(List(long_tokens(1), long_tokens(2))),
-        ProductFeature(List(long_tokens(1), long_tokens(3))),
-        ProductFeature(List(long_tokens(1), long_tokens(4))),
-        ProductFeature(List(long_tokens(2), long_tokens(3))),
-        ProductFeature(List(long_tokens(2), long_tokens(4))),
-        ProductFeature(List(long_tokens(2), long_tokens(5))),
-        ProductFeature(List(long_tokens(3), long_tokens(4))),
-        ProductFeature(List(long_tokens(3), long_tokens(5))),
-        ProductFeature(List(long_tokens(3), long_tokens(6))),
-        ProductFeature(List(long_tokens(4), long_tokens(5))),
-        ProductFeature(List(long_tokens(4), long_tokens(6))),
-        ProductFeature(List(long_tokens(5), long_tokens(6)))
+      //2-skip-tri-grams
+      transform(tokens) shouldBe List(
+        ProductFeature(List(tokens(0), tokens(1), tokens(2))),
+        ProductFeature(List(tokens(0), tokens(1), tokens(3))),
+        ProductFeature(List(tokens(0), tokens(2), tokens(3))),
+        ProductFeature(List(tokens(0), tokens(1), tokens(4))),
+        ProductFeature(List(tokens(0), tokens(3), tokens(4))),
+        ProductFeature(List(tokens(0), tokens(2), tokens(4))),
+        ProductFeature(List(tokens(1), tokens(2), tokens(3))),
+        ProductFeature(List(tokens(1), tokens(2), tokens(4))),
+        ProductFeature(List(tokens(1), tokens(3), tokens(4))),
+        ProductFeature(List(tokens(2), tokens(3), tokens(4)))
       )
     }
   }
