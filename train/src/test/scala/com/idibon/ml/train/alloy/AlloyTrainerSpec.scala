aa81cfba10d0098b93febb516240d9ac6b810797
Stefan Krawczyk <stefan@idibon.com>
2016-02-02 17:58:27 -0800

    Adds Alloy trainers to create multinomial model
    With this we can now orchestrate the creation of an alloy that
    is based on a GangModel that internally has a multinomial LR model
    + per label rules.
    
    Comes with unit test stubs. I assume we'll be changing this around,
    so I didn't fill them in.

---
+++ train/src/test/scala/com/idibon/ml/train/alloy/AlloyTrainerSpec.scala
@@ -0,0 +1,22 @@
+package com.idibon.ml.train.alloy
+
+import org.scalatest._
+
+/**
+  * Tests the Alloy Trainers
+  */
+class AlloyTrainerSpec extends FunSpec
+  with Matchers with BeforeAndAfter with ParallelTestExecution with BeforeAndAfterAll {
+
+  describe("MultiClass1FPRDD tests") {
+    //TODO:
+  }
+
+  describe("MultiClass1FP tests") {
+    //TODO:
+  }
+
+  describe("KClass1FP tests") {
+    //TODO:
+  }
+}
