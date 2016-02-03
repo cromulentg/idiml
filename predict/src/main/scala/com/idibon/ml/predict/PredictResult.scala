d4abb65865a9383f56d5dfed8d14ac465e237048
Stefan Krawczyk <stefan@idibon.com>
2016-02-02 16:37:06 -0800

    Adds Multilabel document result
    Adds a result object that contains results for a bunch of labels.
    This will be used for the upcoming GangModel, since that will
    contain a multinomial model that will span a bunch of labels.
    
    This also adds some accessors to make it easier to work with the
    result objects.

--- predict/src/main/scala/com/idibon/ml/predict/PredictResult.scala
+++ predict/src/main/scala/com/idibon/ml/predict/PredictResult.scala
@@ -1,19 +1,19 @@
 package com.idibon.ml.predict
 
 import scala.collection.mutable
+import scala.collection.JavaConverters._
 
 /**
   * Parent class of all prediction results.
   *
   * Assumptions:
-  *   - Instantiations of this class would be case classes, since case classes are nice.
-  *   - PredictResult is basically a placeholder til we feel out single label vs multiple label and
-  *   the scope of what models/objects deal with the results. Since we might just cleave off worrying
-  *   what high level object type we're dealing with, and instead just use the lower level case class
-  *   throughout.
+  * - Instantiations of this class would be case classes, since case classes are nice.
+  * - PredictResult is basically a placeholder til we feel out single label vs multiple label and
+  * the scope of what models/objects deal with the results. Since we might just cleave off worrying
+  * what high level object type we're dealing with, and instead just use the lower level case class
+  * throughout.
   *
   * @author "Stefan Krawczyk <stefan@idibon.com>"
-  *
   * @param modelIdentifier a string representing a model, right now the class name.
   * @param topLabel
   * @param topLabelProbability
@@ -27,6 +27,30 @@ abstract class PredictResult(modelIdentifier: String,
                              topLabelSignificantFeatures: List[(String, Float)],
                              topLabelMatchCount: Int,
                              topLabelFlags: Map[PredictResultFlag.Value, Boolean]) {
+  /**
+    * Return the top result. This is a single label result for a document.
+    *
+    * @return
+    */
+  def getTopResult(): SingleLabelDocumentResult = {
+    new SingleLabelDocumentResult(
+      modelIdentifier, topLabel, topLabelProbability, topLabelSignificantFeatures,
+      topLabelMatchCount, topLabelFlags)
+  }
+
+  /**
+    * Return all results this represents.
+    *
+    * @return list of results, in order of descending probability
+    */
+  def getAllResults(): java.util.List[SingleLabelDocumentResult]
+
+  /**
+    * Returns the label this result is for.
+    *
+    * @return
+    */
+  def getLabel(): String = topLabel
 }
 
 /**
@@ -35,11 +59,207 @@ abstract class PredictResult(modelIdentifier: String,
 object PredictResultFlag extends Enumeration {
   type PredictResultFlag = Value
   val FORCED // e.g. used when a blacklist or whitelist rule is used.
-    = Value
+  = Value
+}
+
+/**
+  * Class for storing multiple label results for a document. Used by multi-class models.
+  *
+  * @param modelIdentifier a string representing a model, right now the class name.
+  * @param labels
+  * @param probabilities
+  * @param significantFeatures
+  * @param matchCounts
+  * @param flags
+  */
+case class MultiLabelDocumentResult(modelIdentifier: String,
+                                    labels: List[String],
+                                    probabilities: List[Float],
+                                    significantFeatures: List[List[(String, Float)]],
+                                    matchCounts: List[Int],
+                                    flags: List[Map[PredictResultFlag.Value, Boolean]])
+  extends PredictResult(modelIdentifier,
+    labels.head, probabilities.head, significantFeatures.head, matchCounts.head, flags.head)
+    with mutable.Iterable[SingleLabelDocumentResult] {
+  override def iterator: Iterator[SingleLabelDocumentResult] = new MultiLabelDocumentResultIterator(this)
+
+  override def getAllResults(): java.util.List[SingleLabelDocumentResult] = {
+    iterator.toList.asJava
+  }
+}
+
+/**
+  * Iterator to make it easy to go through individual label results.
+  *
+  * @param mldr
+  */
+class MultiLabelDocumentResultIterator(mldr: MultiLabelDocumentResult) extends Iterator[SingleLabelDocumentResult] {
+  private var index = 0
+
+  override def hasNext: Boolean = index < mldr.labels.size
+
+  override def next(): SingleLabelDocumentResult = {
+    if (!hasNext) throw new IndexOutOfBoundsException("No more results to return.")
+    val label = mldr.labels(index)
+    val prob = mldr.probabilities(index)
+    val sigFeatures = mldr.significantFeatures(index)
+    val matchCount = mldr.matchCounts(index)
+    val flags = mldr.flags(index)
+    index += 1
+    val result = new SingleLabelDocumentResultBuilder(mldr.modelIdentifier, label)
+      .addSignificantFeatures(sigFeatures)
+      .setProbability(prob)
+      .addToMatchCount(matchCount)
+    flags.foreach({ case (flag, value) => result.setFlags(flag, value) })
+    result.build()
+  }
+}
+
+/**
+  * Builder to make a MultiLabelDocumentResult.
+  *
+  * @param modelIdentifier
+  * @param labelResults
+  */
+class MultiLabelDocumentResultBuilder(modelIdentifier: String,
+                                      labelResults: Map[String, SingleLabelDocumentResultBuilder]) {
+
+  /**
+    * Alternate constructor.
+    *
+    * @param modelIdentifier
+    * @param labels
+    */
+  def this(modelIdentifier: String, labels: List[String]) = {
+    this(modelIdentifier,
+      labels.map(label =>
+        (label, new SingleLabelDocumentResultBuilder(modelIdentifier, label))).toMap)
+  }
+
+  /**
+    * Sets the probability.
+    *
+    * @param label
+    * @param probability
+    * @return
+    */
+  def setProbability(label: String, probability: Float): MultiLabelDocumentResultBuilder = {
+    labelResults(label).setProbability(probability)
+    this
+  }
+
+  /**
+    * Adds to the current match count.
+    *
+    * @param label
+    * @param matchCount
+    * @return
+    */
+  def addToMatchCount(label: String, matchCount: Int): MultiLabelDocumentResultBuilder = {
+    labelResults(label).addToMatchCount(matchCount)
+    this
+  }
+
+  /**
+    * Sets the match count.
+    *
+    * @param label
+    * @param matchCount
+    * @return
+    */
+  def setMatchCount(label: String, matchCount: Int): MultiLabelDocumentResultBuilder = {
+    labelResults(label).setMatchCount(matchCount)
+    this
+  }
+
+  /**
+    * Appends a feature to this result.
+    *
+    * @param label
+    * @param feature
+    * @return
+    */
+  def addSignificantFeature(label: String, feature: (String, Float)): MultiLabelDocumentResultBuilder = {
+    labelResults(label).addSignificantFeature(feature)
+    this
+  }
+
+  /**
+    * Adds a whole list to this result.
+    *
+    * @param label
+    * @param significantFeatures
+    * @return
+    */
+  def addSignificantFeatures(label: String, significantFeatures: List[(String, Float)]): MultiLabelDocumentResultBuilder = {
+    labelResults(label).addSignificantFeatures(significantFeatures)
+    this
+  }
+
+  /**
+    * Sets flags that we use for decision logic.
+    *
+    * @param label
+    * @param name
+    * @param value
+    * @return
+    */
+  def setFlags(label: String, name: PredictResultFlag.Value, value: Boolean): MultiLabelDocumentResultBuilder = {
+    labelResults(label).setFlags(name, value)
+    this
+  }
+
+  /**
+    * Copies from a previous multi-label document result.
+    *
+    * @param results
+    * @return
+    */
+  def copyFromExistingMultiLabelDocumentResult(results: MultiLabelDocumentResult): MultiLabelDocumentResultBuilder = {
+    // assert labels match
+    require(results.labels.equals(this.labelResults.keys.toList), "Labels should match")
+    results.labels.zipWithIndex.foreach({ case (label, resultIndex) => {
+      val result = this.labelResults(label)
+        .setProbability(results.probabilities(resultIndex))
+        .setSignificantFeatures(results.significantFeatures(resultIndex))
+        .setMatchCount(results.matchCounts(resultIndex))
+      results.flags(resultIndex).foreach({ case (flag, value) => result.setFlags(flag, value) })
+    }
+    })
+    this
+  }
+
+  /**
+    * Builds a MultiLabelDocumentResult object.
+    *
+    * @return
+    */
+  def build(): MultiLabelDocumentResult = {
+    val results = labelResults.map({ case (label, sldrb) => (label, sldrb.build()) })
+    val order = results.map({ case (label, sldr) => (label, sldr.probability) }).toList.sortWith(_._2 < _._2)
+    val baseTuple = (
+      List[String](), //labels
+      List[Float](), //probs
+      List[List[(String, Float)]](), // sig features
+      List[Int](), // match counts
+      List[Map[PredictResultFlag.Value, Boolean]]()) // flags
+    // create list of lists
+    val x = order.foldLeft(baseTuple)({
+        case (lists, (label, prob)) => {
+          (label :: lists._1,
+            prob :: lists._2,
+            results(label).significantFeatures :: lists._3,
+            results(label).matchCount :: lists._4,
+            results(label).flags :: lists._5)
+        }
+      })
+    new MultiLabelDocumentResult(this.modelIdentifier, x._1, x._2, x._3, x._4, x._5)
+  }
 }
 
 /**
   * Class representing a single label prediction for a document.
+  *
   * @param modelIdentifier
   * @param label
   * @param probability
@@ -54,10 +274,13 @@ case class SingleLabelDocumentResult(modelIdentifier: String,
                                      matchCount: Int,
                                      flags: Map[PredictResultFlag.Value, Boolean]) extends PredictResult(
   modelIdentifier, label, probability, significantFeatures, matchCount, flags
-)
+) {
+  override def getAllResults(): java.util.List[SingleLabelDocumentResult] = List(this).asJava
+}
 
 /**
   * Class to build a single label document prediction result.
+  *
   * @param modelIdentifier a string representing a model, right now the class name.
   * @param label
   */
@@ -69,6 +292,7 @@ class SingleLabelDocumentResultBuilder(modelIdentifier: String, label: String) {
 
   /**
     * Sets the probability.
+    *
     * @param probability
     * @return
     */
@@ -79,6 +303,7 @@ class SingleLabelDocumentResultBuilder(modelIdentifier: String, label: String) {
 
   /**
     * Adds to the current match count.
+    *
     * @param matchCount
     * @return
     */
@@ -89,6 +314,7 @@ class SingleLabelDocumentResultBuilder(modelIdentifier: String, label: String) {
 
   /**
     * Sets the match count.
+    *
     * @param matchCount
     * @return
     */
@@ -99,6 +325,7 @@ class SingleLabelDocumentResultBuilder(modelIdentifier: String, label: String) {
 
   /**
     * Appends a feature to this result.
+    *
     * @param feature
     * @return
     */
@@ -109,6 +336,7 @@ class SingleLabelDocumentResultBuilder(modelIdentifier: String, label: String) {
 
   /**
     * Adds a whole list to this result.
+    *
     * @param significantFeatures
     * @return
     */
@@ -118,7 +346,20 @@ class SingleLabelDocumentResultBuilder(modelIdentifier: String, label: String) {
   }
 
   /**
+    * Sets a whole list to this result.
+    *
+    * @param significantFeatures
+    * @return
+    */
+  def setSignificantFeatures(significantFeatures: List[(String, Float)]): SingleLabelDocumentResultBuilder = {
+    this.significantFeatures.clear()
+    this.significantFeatures ++= significantFeatures
+    this
+  }
+
+  /**
     * Sets flags that we use for decision logic.
+    *
     * @param name
     * @param value
     * @return
@@ -130,6 +371,7 @@ class SingleLabelDocumentResultBuilder(modelIdentifier: String, label: String) {
 
   /**
     * Copies from a previous single label result.
+    *
     * @param result
     * @return
     */
@@ -143,6 +385,7 @@ class SingleLabelDocumentResultBuilder(modelIdentifier: String, label: String) {
 
   /**
     * Builds the SingleLabelDocumentResult object.
+    *
     * @return
     */
   def build(): SingleLabelDocumentResult = {
