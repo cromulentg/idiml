f4127f0fc7a17f70c4270b82a866cc0dbe80b6ff
Gary King <gary@idibon.com>
2016-02-05 14:18:13 -0800

    ENG-3127: add trait for optional feature pipelines
    add a new trait, CanHazPipeline, that can be mixed-in to any model
    that may optionally include a feature pipeline. this will replace
    the code in IdibonMulticlassLRModel and IdibonLogisticRegressionModel,
    and be added to GangModel, so that the feature transformation can be
    applied anywhere in the stack
    
    also, tweak the Document type to take advantage of the newly-defined
    methods in FeaturePipeline for inverting the feature transformation;
    the inversion method is passed along with the transformed vector, so
    that subordinate models can return the correct set of significant
    features

---
+++ predict/src/main/scala/com/idibon/ml/predict/CanHazPipeline.scala
@@ -0,0 +1,56 @@
+package com.idibon.ml.predict
+
+import com.idibon.ml.feature.{FeaturePipeline, FeaturePipelineLoader}
+import com.idibon.ml.common.Engine
+import com.idibon.ml.alloy.Alloy
+import org.json4s._
+
+/** Mixin trait for models that may optionally include a FeaturePipeline */
+trait CanHazPipeline {
+
+  def featurePipeline: Option[FeaturePipeline]
+
+  /** Transforms a document using the feature pipeline
+    *
+    * If the current object has a valid feature pipeline, this method will
+    * transform the provided Document using the pipeline and return a new,
+    * transformed Document. If not, the original document is returned
+    * unmodified.
+    *
+    * @param doc Document to transform
+    * @return the transformed document if a transform exists, else doc
+    */
+  def applyPipelineIfPresent(doc: Document): Document = {
+    featurePipeline.map(p => {
+      /* pass the feature pipeline's getFeatureByVector method on to
+       * any models that need to return the original features */
+      Document(doc.json, Some((p(doc.json), p.getFeaturesByVector _)))
+    }).getOrElse(doc)
+  }
+
+  /** Saves the feature pipeline, if present, to an Alloy
+    *
+    * @param writer alloy writer configured for the current object's namespace
+    */
+  def savePipelineIfPresent(writer: Alloy.Writer): JField = {
+    JField(CanHazPipeline.KEY,
+      featurePipeline.flatMap(_.save(writer.within(CanHazPipeline.KEY)))
+        .getOrElse(JNothing))
+  }
+}
+
+object CanHazPipeline {
+  val KEY = "featurePipeline"
+
+  /** Loads a feature pipeline, if one is defined in the current namespace
+    *
+    */
+  def loadPipelineIfPresent(engine: Engine, reader: Option[Alloy.Reader],
+      config: Option[JObject]): Option[FeaturePipeline] = {
+
+    implicit val formats = org.json4s.DefaultFormats
+    val meta = (config.get \ KEY).extract[Option[JObject]]
+    meta.map(pipeConfig => new FeaturePipelineLoader()
+      .load(engine, reader.map(_.within(KEY)), Some(pipeConfig)))
+  }
+}
