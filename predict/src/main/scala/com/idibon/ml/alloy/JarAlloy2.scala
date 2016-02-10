a7a29af1386d06bd3314761de1a01e7325c56522
Gary King <gary@idibon.com>
2016-02-09 16:58:47 -0800

    separate non-JAR bits out of JarAlloy
    the Alloy code was getting a bit messy and hard to extend, due
    to some unclear separation of concerns between the Alloy, the
    Reader and Writer classes. pull quite a bit of alloy structure code
    out from JarAlloy into a new BaseAlloy class (currently called
    BaseAlloy2 until the existing BaseAlloy is removed), so that JarAlloy
    can focus strictly on JAR semantics -- in fact, it probably only
    needs Reader and Writer implementations
    
    also, rather than burying an ever-increasing number of optional
    data points inside the BaseAlloy constructor, refactor the existing
    optional items to separate traits. when furnaces cast alloys, they
    can instantiate anonymous classes with whatever traits are appropriate

---
+++ predict/src/main/scala/com/idibon/ml/alloy/JarAlloy2.scala
@@ -0,0 +1,92 @@
+package com.idibon.ml.alloy
+
+import java.io._
+import java.util.jar._
+import com.idibon.ml.common.Engine
+import com.idibon.ml.predict.PredictResult
+import com.idibon.ml.feature.{Buildable, Builder}
+
+object JarAlloy2 {
+
+  /** Loads a BaseAlloy stored in a JAR, optionally validating the models */
+  def load[T <: PredictResult with Buildable[T, Builder[T]]](
+      engine: Engine, file: File, validate: Boolean): Alloy[T] = {
+    val jar = new JarFile(file)
+    try {
+      val reader = new JarAlloyReader(jar)
+      val alloy = BaseAlloy2.load[T](engine, reader)
+      if (validate) HasValidationData.validate(reader, alloy)
+      alloy
+    } finally {
+      jar.close
+    }
+  }
+
+  /** Saves a BaseAlloy to a JAR */
+  def save[T <: PredictResult with Buildable[T, Builder[T]]](
+      alloy: BaseAlloy2[T], file: File) = {
+    val jos = new JarOutputStream(new FileOutputStream(file))
+    try {
+      val writer = new JarAlloyWriter(jos)
+      alloy.save(writer)
+    } finally {
+      jos.close
+    }
+  }
+}
+
+/** Reads alloy data from a JAR file */
+class JarAlloyReader(jarFile: JarFile) extends Alloy.Reader {
+
+  def within(namespace: String): Alloy.Reader = new SubReader(namespace + "/")
+
+  def resource(name: String): DataInputStream = {
+    val entry = jarFile.getJarEntry(name)
+    if (entry != null) new DataInputStream(jarFile.getInputStream(entry)) else null
+  }
+
+  /** Inner class used for reading internal namespaces */
+  private class SubReader(path: String) extends Alloy.Reader {
+    def within(namespace: String): Alloy.Reader = new SubReader(path + namespace + "/")
+
+    def resource(name: String): DataInputStream = {
+      val entry = jarFile.getJarEntry(path + name)
+      if (entry != null) new DataInputStream(jarFile.getInputStream(entry)) else null
+    }
+  }
+}
+
+/** Writes alloy data to a JAR file */
+class JarAlloyWriter(jarStream: JarOutputStream) extends Alloy.Writer {
+
+  def within(namespace: String): Alloy.Writer = new SubWriter(namespace + "/")
+
+  def resource(name: String): DataOutputStream = new StageStream(new JarEntry(name))
+
+  private class SubWriter(path: String) extends Alloy.Writer {
+    def within(namespace: String): Alloy.Writer = new SubWriter(path + namespace + "/")
+
+    def resource(name: String): DataOutputStream = new StageStream(new JarEntry(path + name))
+  }
+
+  /** Serializes writes to the JAR file
+    *
+    * JAR files entries must be written sequentially, and in entirety,
+    * before advancing to the next entry. Alloy.Writer has no provisions
+    * for this limitation, so resource writes must be staged so that
+    * entire resources are written to the alloy atomically
+    */
+  private class StageStream(entry: JarEntry,
+    stage: ByteArrayOutputStream = new ByteArrayOutputStream)
+      extends DataOutputStream(stage) {
+
+    override def close {
+      super.close
+      JarAlloyWriter.this.synchronized {
+        jarStream.putNextEntry(entry)
+        jarStream.write(stage.toByteArray)
+        jarStream.closeEntry
+      }
+    }
+  }
+}
