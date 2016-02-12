6efc2f8de1ca928949d1d4ec2661f91da7508712
Paul Tepper <paul@idibon.com>
2016-02-12 15:53:41 -0800

    ENG-3184: Add reader for word2vec binary format
    Reader for gzipped binary vector files created by the original word2vec
    C implementation.

---
+++ predict/src/main/scala/com/idibon/ml/feature/word2vec/Word2VecBinReader.java
@@ -0,0 +1,90 @@
+package com.idibon.ml.feature.word2vec;
+
+import java.io.*;
+import java.util.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * Class for reading word2vec gzipped binary vector files and outputting Java hashmaps
+ */
+public class Word2VecBinReader {
+
+    private long words;
+    private long size;
+
+    /**
+     * Converts an ASCII string long integer into a Java long integer
+     *
+     * @param dis DatatInputStream containing a long integer
+     * @return the actual long integer
+     */
+    public long readAsciiLong(DataInputStream dis) {
+        long accum = 0;
+        while(true){
+            try{
+                char c = (char) dis.readByte();
+                if (Character.isWhitespace(c)) {
+                    break;
+                }
+                accum *= 10;
+                accum += Character.getNumericValue(c);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return accum;
+    }
+
+    /**
+     * Parses a gzipped binary file output by the word2vec C implementation
+     *
+     * @param path path to gzipped bin file
+     * @return LinkedHashMap mapping words to vectors
+     */
+    public LinkedHashMap<String, float[]> parseBinFile(String path) {
+        LinkedHashMap<String, float[]> vectors = new LinkedHashMap<>();
+
+        try {
+            DataInputStream data = new DataInputStream(
+                    new BufferedInputStream(
+                        new GZIPInputStream(
+                            new FileInputStream(path))));
+
+            words = readAsciiLong(data);
+            size = readAsciiLong(data);
+
+            if (size > Integer.MAX_VALUE)
+                throw new IllegalArgumentException("Too many dimensions");
+
+            for (long b = 0; b < words; b++) {
+                ByteArrayOutputStream wordBuffer = new ByteArrayOutputStream();
+                for (int c = data.readByte(); c != -1 && c != ' '; c = data.readByte())
+                    wordBuffer.write(c);
+
+                String word = new String(wordBuffer.toByteArray(), "UTF-8");
+                float[] vector = new float[(int) size];
+                /* the original implementation generally runs on little-endian
+                 * native architectures, and there is no accommodation for big-
+                 * endian in the file format, so just assume that an endian-
+                 * swap is necessary to get the float values into Java */
+                byte[] swap = new byte[4];
+                ByteBuffer endianConverter = ByteBuffer.wrap(swap).order(ByteOrder.LITTLE_ENDIAN);
+
+                for (int i = 0; i < (int) size; i++) {
+                    data.readFully(swap);
+                    vector[i] = endianConverter.getFloat();
+                    endianConverter.rewind();
+                }
+
+                vectors.put(word.trim(), vector);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return vectors;
+    }
+
+}
