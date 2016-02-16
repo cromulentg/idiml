92556395046565573acf4cf4458cdaf8d8fb9d6f
Stefan Krawczyk <stefan@idibon.com>
2016-02-16 10:25:11 -0800

    Reorders MetricTypes enum to be alphabetical
    Since that looks more ordered and easier to find a metric if it's
    alphabetical. This shouldn't pose a backwards compatiability problem
    since we don't actually use the actual int enum value, but instead
    the name, which would deserialize to the right thing.

--- predict/src/main/scala/com/idibon/ml/predict/ml/metrics/MetricTypes.java
+++ predict/src/main/scala/com/idibon/ml/predict/ml/metrics/MetricTypes.java
@@ -13,57 +13,42 @@ package com.idibon.ml.predict.ml.metrics;
  */
 public enum MetricTypes {
     /**
-     * This is reserved for the threshold that results in the Best F1.
-     * Used for tuning binary classifiers.
+     * The over accuracy of our model.
      */
-    BestF1Threshold(FloatMetric.class),
+    Accuracy(FloatMetric.class),
     /**
      * Value for the area under the receiver operating charactistic curve.
      */
     AreaUnderROC(FloatMetric.class),
     /**
-     * Points to plot the receiver operating characteristic.
+     * This is reserved for the threshold that results in the Best F1.
+     * Used for tuning binary classifiers.
      */
-    ReceiverOperatingCharacteristic(PointsMetric.class),
+    BestF1Threshold(FloatMetric.class),
     /**
-     * Points to plot the precision - recall curve.
-     * Useful for showing the trade-off between the two.
+     * Points to create a confusion matrix.
      */
-    PrecisionRecallCurve(PointsMetric.class),
-    /**
-     * Points to plot F1 by decision threshold.
-     */
-    F1ByThreshold(PointsMetric.class),
-    /**
-     * Points to plot precision by decision threshold.
-     */
-    PrecisionByThreshold(PointsMetric.class),
-    /**
-     * Points to plot recall by decision threshold.
-     */
-    RecallByThreshold(PointsMetric.class),
-    /**
-     * The precision as a float. When we call something
-     * foo, how accurate are we?
-     */
-    Precision(FloatMetric.class),
-    /**
-     * The recall as a float. Out of all things foo,
-     * how many of them do we correctly identify?
-     */
-    Recall(FloatMetric.class),
+    ConfusionMatrix(ConfusionMatrixMetric.class),
     /**
      * The harmonic mean between precision and recall.
      */
     F1(FloatMetric.class),
     /**
-     * Precision but for an individual label.
+     * Points to plot F1 by decision threshold.
      */
-    LabelPrecision(LabelFloatMetric.class),
+    F1ByThreshold(PointsMetric.class),
     /**
-     * Recall but for an individual label.
+     * The hamming loss. A metric for the multi-label case.
      */
-    LabelRecall(LabelFloatMetric.class),
+    HammingLoss(FloatMetric.class),
+    /**
+     * Properties related to fitting of a model.
+     */
+    HyperparameterProperties(PropertyMetric.class),
+    /**
+     * Count of data points for that label.
+     */
+    LabelCount(LabelIntMetric.class),
     /**
      * F1 but for an individual label.
      */
@@ -73,37 +58,13 @@ public enum MetricTypes {
      */
     LabelFPR(LabelFloatMetric.class),
     /**
-     * Precision weighted by the number of items in
-     * each class (i.e. label)
+     * Precision but for an individual label.
      */
-    WeightedPrecision(FloatMetric.class),
+    LabelPrecision(LabelFloatMetric.class),
     /**
-     * Recall weighted by the number of items in
-     * each class (i.e. label)
+     * Recall but for an individual label.
      */
-    WeightedRecall(FloatMetric.class),
-    /**
-     * F1 weighted by the number of items in
-     * each class (i.e. label)
-     */
-    WeightedF1(FloatMetric.class),
-    /**
-     * False positive rate weighted by the number of items in
-     * each class (i.e. label)
-     */
-    WeightedFPR(FloatMetric.class),
-    /**
-     * Points to create a confusion matrix.
-     */
-    ConfusionMatrix(ConfusionMatrixMetric.class),
-    /**
-     * Count of data points for that label.
-     */
-    LabelCount(LabelIntMetric.class),
-    /**
-     * Properties related to fitting of a model.
-     */
-    HyperparameterProperties(PropertyMetric.class),
+    LabelRecall(LabelFloatMetric.class),
     /**
      * The average of individual label F1s in the non-mutually
      * exclusive case.
@@ -120,17 +81,56 @@ public enum MetricTypes {
      */
     MicroRecall(FloatMetric.class),
     /**
-     * The haming loss. A metric for the mutli-label case.
+     * The precision as a float. When we call something
+     * foo, how accurate are we?
      */
-    HammingLoss(FloatMetric.class),
+    Precision(FloatMetric.class),
+    /**
+     * Points to plot precision by decision threshold.
+     */
+    PrecisionByThreshold(PointsMetric.class),
+    /**
+     * Points to plot the precision - recall curve.
+     * Useful for showing the trade-off between the two.
+     */
+    PrecisionRecallCurve(PointsMetric.class),
+    /**
+     * The recall as a float. Out of all things foo,
+     * how many of them do we correctly identify?
+     */
+    Recall(FloatMetric.class),
+    /**
+     * Points to plot recall by decision threshold.
+     */
+    RecallByThreshold(PointsMetric.class),
+    /**
+     * Points to plot the receiver operating characteristic.
+     */
+    ReceiverOperatingCharacteristic(PointsMetric.class),
     /**
      * How accurate each label is in the non-mutually exclusive case.
      */
     SubsetAccuracy(FloatMetric.class),
     /**
-     * The over accuracy of our model.
+     * F1 weighted by the number of items in
+     * each class (i.e. label)
      */
-    Accuracy(FloatMetric.class);
+    WeightedF1(FloatMetric.class),
+    /**
+     * False positive rate weighted by the number of items in
+     * each class (i.e. label)
+     */
+    WeightedFPR(FloatMetric.class),
+    /**
+     * Precision weighted by the number of items in
+     * each class (i.e. label)
+     */
+    WeightedPrecision(FloatMetric.class),
+    /**
+     * Recall weighted by the number of items in
+     * each class (i.e. label)
+     */
+    WeightedRecall(FloatMetric.class);
 
     MetricTypes(Class<? extends RawMetric> dataType) {
         this.dataType = dataType;
