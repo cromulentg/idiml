5b19261ef311a1554bfcad5be46010659c36495d
Gary King <gary@idibon.com>
2016-03-05 13:53:54 -0800

    ENG-1811: furnace for BIOModel NER models
    Furnace2 implementation for training BIOModels using a sequence generator
    and feature extractor loaded from a JSON trainer configuration

---
+++ train/src/main/scala/com/idibon/ml/train/furnace/ChainNERFurnace.scala
@@ -0,0 +1,93 @@
+package com.idibon.ml.train.furnace
+
+import scala.util.Random
+
+import com.idibon.ml.predict._
+import com.idibon.ml.feature._
+import com.idibon.ml.common.Engine
+import com.idibon.ml.train.TrainOptions
+import com.idibon.ml.train.datagenerator.crf._
+
+import org.json4s._
+
+/** Furnace for generating Chain NER models using ConLL-style BIO tags
+  *
+  * @param name Name of the furnace (and the generated model within the Alloy)
+  * @param sequenceGenerator sequence generator, must not be frozen
+  * @param featureExtractor feature extractor, must not be frozen
+  * @param prng random number generator instance to use
+  */
+class ChainNERFurnace(val name: String,
+  override val sequenceGenerator: SequenceGenerator,
+  override val featureExtractor: ChainPipeline,
+  private[furnace] val prng: Random) extends Furnace2[Span] with BIOTagger {
+
+  /** Trains the model
+    *
+    * @param options training options and data
+    * @return the trained model
+    */
+  protected def doTrain(options: TrainOptions): PredictModel[Span] = {
+    /* prime the feature pipelines, and return the total number of
+     * dimensions in the feature space (size of the last vector in the
+     * last extant chain) */
+    val dimensions = options.documents().foldLeft(0)((dims, doc) => {
+      /* if the last document(s) all convert to empty sequences, return
+       * the previous dimension value */
+      featureExtractor(doc, sequenceGenerator(doc))
+        .lastOption
+        .map(_.value.size)
+        .getOrElse(dims)
+    })
+    sequenceGenerator.freeze()
+    featureExtractor.freeze()
+
+    // TODO: figure out how to pass regularization parameters...
+    val model = new crf.FactorieCRF(dimensions) with crf.TrainableFactorieModel
+
+    /* generate training data by tagging all of the documents with the
+     * appropriate B/I/O sequence, and observing everything in the model */
+    val trainingData = options.documents().map(json => {
+      model.observe(tag(json).map({ case (t, v) => (t.toString, v) }))
+    }).toSeq
+
+    model.train(trainingData, prng)
+
+    new crf.BIOModel(model, sequenceGenerator, featureExtractor)
+  }
+}
+
+object ChainNERFurnace {
+
+  /** Constructs a ChainNERFurnace to train NER models
+    *
+    * @param name furnace / model name
+    * @param engine current IdiML engine content
+    * @param json JSON configuration data for the CRFFurnace
+    */
+  def apply(name: String, engine: Engine, json: JObject): ChainNERFurnace = {
+    implicit val formats = org.json4s.DefaultFormats
+    val config = json.extract[ChainNERFurnaceConfig]
+
+    val prng = new Random(config.seed.getOrElse(
+      java.lang.Float.floatToIntBits(math.Pi.toFloat)))
+
+    val sequenceGenerator = (new SequenceGeneratorLoader).load(engine,
+      None, Some(config.sequenceGenerator))
+
+    val featureExtractor = (new ChainPipelineLoader).load(engine,
+      None, Some(config.featureExtractor))
+
+    new ChainNERFurnace(name, sequenceGenerator, featureExtractor, prng)
+  }
+}
+
+/** Configuration schema for the CRFFurnace
+  *
+  * @param sequenceGenerator configuration of model sequence generator
+  * @param featureExtractor configuration of model feature extractor
+  * @param seed optional seed value for random number generator
+  */
+case class ChainNERFurnaceConfig(sequenceGenerator: JObject,
+  featureExtractor: JObject,
+  seed: Option[Int])
