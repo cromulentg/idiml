811552d2c3c292d141de507913cee68261b39104
nlgaylord <nick@idibon.com>
2016-03-22 19:05:41 -0700

    ENG-3219 Updated StopwordsTranformer and unit tests

--- predict/src/main/scala/com/idibon/ml/feature/stopwords/StopwordsTransformer.scala
+++ predict/src/main/scala/com/idibon/ml/feature/stopwords/StopwordsTransformer.scala
@@ -9,10 +9,23 @@ import com.idibon.ml.feature.language.LanguageCode
 import scala.io.Source
 
 /**
-  * Created by nick on 3/18/16.
+  * Created by nick gaylord on 3/18/16.
+  *
+  * Stopwords are frequent tokens, listed in separate language-specific files,
+  * that model builders may want to exclude from being counted as unigram
+  * features. This transformer takes a sequence of Words and returns another
+  * sequence of Words filtered to exclude the contents of the stopword file
+  * corresponding to the document's LanguageCode
   */
 class StopwordsTransformer extends com.idibon.ml.feature.FeatureTransformer {
+  val stopwordMap = generateStopFileMap()
 
+  /**
+    * Get the contents of the stopword file directory
+    *
+    * @param dir -- the directory containing the various stopword files
+    * @return -- returns a list of filenames in dir
+    */
   def getListOfFiles(dir: String):List[File] = {
     val d = new File(dir)
     if (d.exists && d.isDirectory) {
@@ -22,29 +35,47 @@ class StopwordsTransformer extends com.idibon.ml.feature.FeatureTransformer {
     }
   }
 
-  
+  /**
+    * Iterate over a list of stopword files and extract their contents,
+    * normalizing filenames to three-letter ISO 639-2 language codes
+    *
+    * @return -- a map from language codes to language-specific stopword lists
+    */
+  def generateStopFileMap() = {
+    val stopFileDirectory = getClass.getClassLoader.getResource("stopwords/").getPath()
+    val stopFileList = getListOfFiles(stopFileDirectory)
+    val stopwordLists = stopFileList.map(x => {
+      val stopwordSet = Source.fromFile(x).getLines().toList.toSet
+      val twoLetterCode = x.getName.split("\\.")(0)
+      val threeLetterCode = LanguageCode.normalize(twoLetterCode)
+      (threeLetterCode, stopwordSet)
+    }).collect({ case (Some(threeLetterCode), sws) => (threeLetterCode, sws) })
+    stopwordLists.toMap
+  }
 
-  val stopFile = "/Users/nick/github/idibin/fixtures/stopwords/en.txt"
-  val englishStopSet = Source.fromFile(stopFile).getLines().toList.toSet
-  val stopwordMap = Map("eng" -> englishStopSet)
-
+  /**
+    * Word-by-word, identify as a stopword or not
+    *
+    * @param x -- an individual word
+    * @param stops -- the stopword list corresponding to the document's language code
+    * @return -- Boolean, 1 if the word is not in the stopword list, 0 if it is
+    */
   def filter(x: Word, stops: Set[String]) = {
     !stops.contains(x.word.toLowerCase)
   }
 
+  /**
+    * Take a sequence of words (a document) and return a sequence of words
+    * with stopwords removed.
+    *
+    * @param input -- a sequence of Words
+    * @param lc -- the document's LanguageCode
+    * @return -- a sequence of Words that is a (not necessarily proper) subset of input
+    */
   def apply(input: Seq[Word], lc: Feature[LanguageCode]): Seq[Word] = {
-    val stopSet = stopwordMap.getOrElse(lc.get.iso_639_2.get, Set())
+    val code: LanguageCode = lc.get
+    val stopSet = stopwordMap.getOrElse(code.iso_639_2.getOrElse(""), Set())
     input.filter(x => filter(x, stopSet))
-  }
-
-  def loadStopWords(dir: String) = {
-    // load files
-
-    // for each file:
-     // create stop words set
-     // create 3 letter language code
-
-    // create map
   }
 
 }
--- predict/src/test/scala/com/idibon/ml/feature/stopwords/StopwordsSpec.scala
+++ predict/src/test/scala/com/idibon/ml/feature/stopwords/StopwordsSpec.scala
@@ -1,8 +1,71 @@
 package com.idibon.ml.feature.stopwords
 
+import com.idibon.ml.feature.language.LanguageCode
+import org.scalatest.{Matchers, FunSpec}
+import com.idibon.ml.feature.bagofwords.Word
+
 /**
   * Created by nick on 3/21/16.
   */
-class StopwordsSpec {
+class StopwordsSpec extends FunSpec with Matchers {
 
+  val transform = new StopwordsTransformer()
+
+  describe("Should work on an empty sequence") {
+    it("should work on an empty sequence") {
+      transform(Seq[Word](), LanguageCode((Some("eng")))) shouldBe empty
+    }
+  }
+
+  describe("The cat in the hat --> cat, hat") {
+    it("should remove 'The', 'in', and 'the'") {
+      val sentence = List(
+        Word("The"),
+        Word("cat"),
+        Word("in"),
+        Word("the"),
+        Word("hat")
+      )
+      val expected = List(new Word("cat"), new Word("hat"))
+      transform(sentence, LanguageCode(Some("eng"))) shouldBe expected
+    }
+  }
+
+  describe("Il nome della rosa --> nome, rosa") {
+    it("should remove Il, della") {
+      val sentence = List(
+        Word("Il"),
+        Word("nome"),
+        Word("della"),
+        Word("rosa")
+      )
+      val expected = List(new Word("nome"), new Word("rosa"))
+      transform(sentence, LanguageCode(Some("ita"))) shouldBe expected
+    }
+  }
+
+  describe("Properly handle unrepresented/bad language code") {
+    it("should return the sentence as is") {
+      val sentence = List(
+        Word("The"),
+        Word("cat"),
+        Word("in"),
+        Word("the"),
+        Word("hat")
+      )
+      val expected = sentence
+      transform(sentence, LanguageCode(Some("day"))) shouldBe expected
+    }
+    it("should return the sentence as is also") {
+      val sentence = List(
+        Word("The"),
+        Word("cat"),
+        Word("in"),
+        Word("the"),
+        Word("hat")
+      )
+      val expected = sentence
+      transform(sentence, LanguageCode(None)) shouldBe expected
+    }
+  }
 }
