c3087c151e4ec805b47afc9469b1cf8063c2a4d2
Stefan Krawczyk <stefan@idibon.com>
2016-04-04 17:57:42 -0700

    Fixes up stopword transformer loading
    This adds the ability to load a transformer from the resources
    for training, and then loading them from an alloy for prediction.
    Adds a unit test for saving & loading, also creates analogous test
    resources so that the unit tests work too.
    
    The idea here is that we only save custom stopwords to the alloy
    that differ from our default set of stopwords which will live in
    the jar that the stopwords transformer depends on.
    
    Also we only for now support loading custom stopwords from a local
    file, as opposed to a web or S3 resoruce, etc.
    
    Note: stopword files require the precomposed versions of letters,
    that way when we normalize them, we'll also include the decomposed
    variant.

--- predict/src/test/scala/com/idibon/ml/feature/stopwords/StopwordsTransformerSpec.scala
+++ predict/src/test/scala/com/idibon/ml/feature/stopwords/StopwordsTransformerSpec.scala
@@ -1,15 +1,20 @@
 package com.idibon.ml.feature.stopwords
 
+import com.idibon.ml.alloy.{MemoryAlloyReader, MemoryAlloyWriter}
+import com.idibon.ml.common.EmbeddedEngine
 import com.idibon.ml.feature.language.LanguageCode
+import org.json4s.JsonAST.{JString, JObject}
 import org.scalatest.{Matchers, FunSpec}
 import com.idibon.ml.feature.bagofwords.Word
+
+import scala.collection.mutable
 
 /**
   * Created by nick on 3/21/16.
   */
 class StopwordsTransformerSpec extends FunSpec with Matchers {
 
-  val transform = new StopwordsTransformer()
+  val transform = new StopwordsTransformerLoader().load(null, None, None)
 
   describe("Should work on an empty sequence") {
     it("should work on an empty sequence") {
@@ -36,6 +41,18 @@ class StopwordsTransformerSpec extends FunSpec with Matchers {
       val sentence = List(
         Word("questo"),
         Word("è"),
+        Word("un"),
+        Word("segno"),
+        Word("diacritico")
+      )
+      val expected = List(new Word("segno"), new Word("diacritico"))
+      transform(sentence, LanguageCode(Some("ita"))) shouldBe expected
+    }
+
+    it("should properly handle decomposed characters") {
+      val sentence = List(
+        Word("questo"),
+        Word("\u0065\u0301"),
         Word("un"),
         Word("segno"),
         Word("diacritico")
@@ -82,4 +99,64 @@ class StopwordsTransformerSpec extends FunSpec with Matchers {
       transform(sentence, LanguageCode(None)) shouldBe expected
     }
   }
+
+  describe("save & load tests") {
+    it("saves and loads defaults") {
+      val stopwords = Map(
+        "ita" -> Set("IL", "UN", "della", "il", "un", "questo", "QUESTO", "È", "DELLA", "è"),
+        "eng" -> Set("IN", "in", "THE", "A", "a", "AND", "and", "the")
+      )
+      val transform = new StopwordsTransformer(stopwords)
+      // Save the results
+      val archive = mutable.HashMap[String, Array[Byte]]()
+      transform.save(new MemoryAlloyWriter(archive))
+
+      val transform2 = (new StopwordsTransformerLoader).load(
+        new EmbeddedEngine, Some(new MemoryAlloyReader(archive.toMap)), None)
+
+      transform2.stopwordMap shouldBe transform.stopwordMap
+    }
+    it("saves and loads custom entries") {
+      val stopwords = Map(
+        "ita" -> Set("IL", "UN", "della", "il", "un", "questo", "QUESTO", "È", "DELLA", "è"),
+        "eng" -> Set("a", "the", "is"),
+        "jpn" -> Set("は"))
+      val transform = new StopwordsTransformer(stopwords)
+      // Save the results
+      val archive = mutable.HashMap[String, Array[Byte]]()
+      transform.save(new MemoryAlloyWriter(archive))
+
+      val transform2 = (new StopwordsTransformerLoader).load(
+        new EmbeddedEngine, Some(new MemoryAlloyReader(archive.toMap)), None)
+
+      transform2.stopwordMap shouldBe transform.stopwordMap
+    }
+
+    it("loads custom entries from config that override defaults & saving and loading works") {
+      val resourceFile = getClass.getClassLoader.getResource("fixtures/extra_stopwords.txt")
+      val config = JObject(List(
+        ("languages", JObject(List(
+          ("eng", JString(s"file://${resourceFile.getFile()}")),
+          ("jpn", JString(s"file://${resourceFile.getFile()}"))
+        )))))
+      val transform = (new StopwordsTransformerLoader).load(
+        new EmbeddedEngine, None, Some(config))
+      transform.stopwordMap shouldBe Map(
+        "eng" -> Set("hello", "this", "is", "a", "test"),
+        "ita" -> Set("É", "IL", "UN", "è", "é", "É", "della", "é", "il", "un", "È",
+          "questo", "QUESTO", "È", "DELLA", "è"),
+        "jpn" -> Set("hello", "this", "is", "a", "test")
+      )
+      val archive = mutable.HashMap[String, Array[Byte]]()
+      transform.save(new MemoryAlloyWriter(archive))
+
+      val transform2 = (new StopwordsTransformerLoader).load(
+        new EmbeddedEngine, Some(new MemoryAlloyReader(archive.toMap)), None)
+
+      transform2.stopwordMap shouldBe transform.stopwordMap
+    }
+  }
 }
+
+
+
